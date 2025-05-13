23079738d4d22ae52a51f73069277cea8d376569
David <davidesparzaj22@gmail.com>
2025-05-13 17:04:44 -0500

    feat(frontend/src/services/commands.js): add getByCommand service method in commands.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,32 @@
 import { useState } from "react";
 import { Form } from "./components/input/Form";
+import { getCommand } from "./services/commands";
 
 function App() {
-  const [inputText, setInputText] = useState('')
-  const [commands, setCommands] = useState([])
+  const [inputText, setInputText] = useState("");
+  const [commands, setCommands] = useState([]);
 
-  const handleInput = (e) => {  
-    setInputText(e.target.value)
-  }
+  const handleInput = (e) => {
+    setInputText(e.target.value);
+    if (e === "Enter") {
+      getCommand(inputText).then((cmd) => {
+        console.log("promise fulfilled");
+        setCommands(cmd)
+        setInputText("")
+      });
+    }
+  };
 
-  
-  return (<div> 
-    <h1>Commander Terminal</h1>
-    <Form onChange={handleInput} value={inputText} />
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+  };
 
-  </div>)
+  return (
+    <div>
+      <h1>Commander Terminal</h1>
+      <Form onChange={handleInput} value={inputText} />
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/services/commands.js
+++ frontend/src/services/commands.js
@@ -1,1 +1,8 @@
 import axios from "axios";
+const URL = import.meta.env.REACT_URL
+
+export const getCommand = async (cmd) => {
+    const request = axios.get(`${URL}/${encodeURIComponent(cmd)}`)
+    const res = await request;
+    return res.data;
+}
