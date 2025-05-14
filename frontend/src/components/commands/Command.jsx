b782f8d0683cc31297be6bc59fac1a39e26ba320
David <davidesparzaj22@gmail.com>
2025-05-14 10:09:58 -0500

    feat(frontend/src/components/commands/Command.jsx) implement copy-to-clipboard button logic in Command.jsx

--- frontend/src/components/commands/Command.jsx
+++ frontend/src/components/commands/Command.jsx
@@ -1,15 +1,28 @@
-export const CommandList = ({command}) => {
-    const Button = () => {
-        return(
-            <>
-            <button>Copy text</button>
-            </>
-        )
-    }
-    return (
-        <div>
-            <p>{command.text}</p>
-            <Button/>
-        </div>
-    )
-}
+const Button = ({text}) => {    
+  const handleClick = (e) => {
+    const btn = e.currentTarget;
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        btn.textContent = "Copied!";
+        setTimeout(() => {
+          btn.textContent = "Copy text";
+        }, 2000);
+      })
+      .catch((err) => console.error(err));
+  };
+  return (
+    <>
+      <button onClick={handleClick}>Copy text</button>
+    </>
+  );
+};
+
+export const CommandList = ({ command }) => {
+  return (
+    <div>
+      <p>{command.text}</p>
+      <Button text ={command.text} />
+    </div>
+  );
+};
