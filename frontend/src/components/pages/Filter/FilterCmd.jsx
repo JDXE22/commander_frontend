4c01dd9b334830e9896fe28d963efa347cdcedbc
David <davidesparzaj22@gmail.com>
2025-05-15 14:39:22 -0500

    feat(frontend/src/components/pages/Filter/ FilterCmd.jsx): create FilterCmd component to display filtered commands view

--- frontend/src/components/pages/Filter/FilterCmd.jsx
+++ frontend/src/components/pages/Filter/FilterCmd.jsx
@@ -1,8 +1,31 @@
-export const FilterCmd = () => { 
-    return (
-        <div>
-            <h3>Filter all Commands</h3>
-            
-        </div>
-    )
-}
+export const FilterCmd = ({ value, handle }) => {
+  const [filter, setFilter] = useState("");
+  const [filteredCommands, setFilteredCommands] = useState([]);
+
+  const handleCommand = (e) => {
+    setFilter(e.target.value);
+    const filtered = value.filter((cmd) => {
+      return cmd.toLowerCase().includes(e.target.value.toLowerCase());
+    });
+    handle(filtered);
+    setFilteredCommands(filtered);
+  };
+
+  return (
+    <div>
+      <h3>Filter all Commands</h3>\
+      <input value={value} onChange={handleCommand} />
+      <div>
+        {filteredCommands.lenght > 0 ? (
+          <ul>
+            {filteredCommands.map((cmd) => (
+              <li key={cmd.id}>{cmd}</li>
+            ))}
+          </ul>
+        ) : (
+          "No commands found"
+        )}
+      </div>
+    </div>
+  );
+};
