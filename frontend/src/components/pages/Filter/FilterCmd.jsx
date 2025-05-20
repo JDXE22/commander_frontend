5c0457c7c01b6d1f89ed423e7b2361660bacd5d4
David <davidesparzaj22@gmail.com>
2025-05-20 14:59:05 -0500

    chore(frontend/src/components/pages/Filter
    /FilterCmd.jsx):
    
    remove unused form and input elements
    
    from Filter page

--- frontend/src/components/pages/Filter/FilterCmd.jsx
+++ frontend/src/components/pages/Filter/FilterCmd.jsx
@@ -5,49 +5,40 @@ const Button = ({ handle, text }) => {
   return <button onClick={handle}>{text}</button>;
 };
 
-export const FilterCmd = ({ value, handle, commands }) => {
-  const [filter, setFilter] = useState("");
+export const FilterCmd = () => {
   const [filteredCommands, setFilteredCommands] = useState([]);
   const [page, setPage] = useState(1);
 
-  useEffect(() => {
-    getCommands({page: page}).then((cmds) => {
-      setFilteredCommands(cmds);
-    }).catch((err) => {
-      setFilteredCommands([]);
-    });
-  }, [page]) ;
-
-  const handleForm = (e) => {
-    e.preventDefault();
+  const commands = () => {
+    getCommands({ page: page })
+      .then((cmds) => {
+        setFilteredCommands(cmds);
+      })
   };
 
-  const handleCommand = (e) => {
-    setFilter(e.target.value);
-  };
+  useEffect(commands, [page]);
 
   return (
     <div className="filter">
       <h3>Filter all Commands</h3>
-      <form onSubmit={handleForm}>
-        <input onChange={handleCommand} />
-        <div>
-          <ul className="commands">
-            {filteredCommands.length > 0 ? (
-              <ul>
-                {filteredCommands.map((cmd) => (
-                  <li key={cmd._id}>
-                    {cmd.command} <br />
-                    {cmd.text}
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              "No commands found"
-            )}
-          </ul>
-        </div>
-      </form>
+
+      <div>
+        <ul className="commands">
+          {filteredCommands.length > 0 ? (
+            <ul>
+              {filteredCommands.map((cmd) => (
+                <li key={cmd._id}>
+                  {cmd.command} <br />
+                  {cmd.text}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            "No commands found"
+          )}
+        </ul>
+      </div>
+
       <div>
         <Button handle={() => setPage(page - 1)} text="Previous" />
         <Button handle={() => setPage(page + 1)} text="Next" />
