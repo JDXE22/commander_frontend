37c3781622352cb26167969bf1be047df8690085
David <davidesparzaj22@gmail.com>
2025-06-03 12:25:44 -0500

    refactor(frontend/src/shared/ui/Form/Form.jsx):
    move Form component to shared/ui/Form
    
    for better UI organization

--- frontend/src/features/commands/lookup/Home.jsx
+++ frontend/src/features/commands/lookup/Home.jsx
@@ -1,5 +1,5 @@
 import { CommandList } from "../list/Command";
-import { Form } from "../create/Form";
+import { Form } from "../../../shared/ui/Form/Form";
 
 export const Home = ({
   handleInput,
