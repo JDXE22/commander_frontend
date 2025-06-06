ed210b8f0eb6d804dd7736c9b48e5a79340e4146
David <davidesparzaj22@gmail.com>
2025-06-06 13:57:47 -0500

    fix(frontend/src/features/commands/list
    /Command.jsx):
    
    update CopyButton prop to textToCopy to enable
    
    proper copy functionality

--- frontend/src/features/commands/list/Command.jsx
+++ frontend/src/features/commands/list/Command.jsx
@@ -7,7 +7,7 @@ export const CommandList = ({ command, className = "" }) => {
         <div key={cmd._id} >
           <span className="commandText">{cmd.text}</span>
           <div className="commandActions">
-            <CopyButton text={cmd.text} />
+            <CopyButton textToCopy={cmd.text} />
           </div>
         </div>
       ))}
